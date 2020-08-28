1d6c6d0dfe5e78f8661970a40b39d8508227d5f9
Masamichi Hosoda <trueroad@trueroad.jp>
2020-08-28 19:10:47 +0900

    Use lexical-binding for lisp/tr-ime-module-helper.el

--- lisp/tr-ime-module-helper.el
+++ lisp/tr-ime-module-helper.el
@@ -1,4 +1,4 @@
-;; -*- mode: emacs-lisp; coding: utf-8 -*-
+;; -*- mode: emacs-lisp; coding: utf-8; lexical-binding: t -*-
 
 ;; This file is part of
 ;; Simple IME module for GNU Emacs (tr-emacs-ime-module)
