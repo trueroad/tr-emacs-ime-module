b1814523528baab1eaaa6b6eb2a8c6f5ace4b8ff
Masamichi Hosoda <trueroad@trueroad.jp>
2020-09-06 11:39:58 +0900

    Add tr-ime-module2 registered message "WM_TR_IME_SUBCLASSIFY"
    サブクラス化対象の HWND に対して post するメッセージを登録する。

---
+++ src/message.hh
@@ -0,0 +1,32 @@
+// -*- mode: c++; coding: utf-8 -*-
+
+// This file is part of
+// Simple IME module for GNU Emacs (tr-emacs-ime-module)
+// https://github.com/trueroad/tr-emacs-ime-module
+//
+// Copyright (C) 2020 Masamichi Hosoda
+//
+// Simple IME module for GNU Emacs (tr-emacs-ime-module)
+// is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Simple IME module for GNU Emacs (tr-emacs-ime-module)
+// is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with tr-emacs-ime-module.
+// If not, see <https://www.gnu.org/licenses/>.
+
+#ifndef INCLUDE_GUARD_MESSAGE_HH
+#define INCLUDE_GUARD_MESSAGE_HH
+
+#include <windows.h>
+
+extern const UINT u_WM_TR_IME_SUBCLASSIFY_;
+
+#endif // INCLUDE_GUARD_MESSAGE_HH
