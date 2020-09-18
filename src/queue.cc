4640d25b680bad94911ea38f36b3f63b0ddc2ba6
Masamichi Hosoda <trueroad@trueroad.jp>
2020-09-18 21:59:59 +0900

    Add Module2 src/queue.{cc|hh}
    UI スレッドから Lisp スレッドへの通知用キューを追加。

---
+++ src/queue.cc
@@ -0,0 +1,110 @@
+// -*- mode: c++; coding: utf-8 -*-
+
+// This file is part of
+// Simple IME module for GNU Emacs (tr-emacs-ime-module)
+// https://github.com/trueroad/tr-emacs-ime-module
+//
+// Copyright (C) 2020 Masamichi Hosoda
+//
+// Simple IME module for GNU Emacs (tr-emacs-ime-module)
+// is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Simple IME module for GNU Emacs (tr-emacs-ime-module)
+// is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with tr-emacs-ime-module.
+// If not, see <https://www.gnu.org/licenses/>.
+
+#include "queue.hh"
+
+#include <algorithm>
+#include <deque>
+#include <memory>
+#include <mutex>
+
+std::mutex ui_to_lisp_queue::mtx_;
+std::deque<std::unique_ptr<queue_message>> ui_to_lisp_queue::queue_;
+
+bool
+ui_to_lisp_queue::empty (void)
+{
+  std::lock_guard<std::mutex> lock (mtx_);
+
+  return queue_.empty ();
+}
+
+void
+ui_to_lisp_queue::enqueue (std::unique_ptr<queue_message> &&m)
+{
+  std::lock_guard<std::mutex> lock (mtx_);
+
+  queue_.push_back (std::move (m));
+}
+
+void
+ui_to_lisp_queue::enqueue_fast (std::unique_ptr<queue_message> &&m)
+{
+  std::lock_guard<std::mutex> lock (mtx_);
+
+  queue_.push_front (std::move (m));
+}
+
+void
+ui_to_lisp_queue::enqueue_one (std::unique_ptr<queue_message> &&m)
+{
+  std::lock_guard<std::mutex> lock (mtx_);
+
+  if (std::find_if (queue_.begin (), queue_.end (),
+                    [&m] (const std::unique_ptr<queue_message> &p)
+                    {
+                      return p->get_message () == m->get_message ();
+                    }
+                    ) == queue_.end ())
+    queue_.push_back (std::move (m));
+}
+
+void
+ui_to_lisp_queue::enqueue_fast_one (std::unique_ptr<queue_message> &&m)
+{
+  std::lock_guard<std::mutex> lock (mtx_);
+
+  while (!queue_.empty ())
+    {
+      auto it = std::find_if (queue_.begin (), queue_.end (),
+                              [&m] (const std::unique_ptr<queue_message> &p)
+                              {
+                                return p->get_message () == m->get_message ();
+                              }
+                              );
+      if (it == queue_.end ())
+        break;
+      queue_.erase (it);
+    }
+
+  queue_.push_front (std::move (m));
+}
+
+std::unique_ptr<queue_message>
+ui_to_lisp_queue::dequeue (void)
+{
+  std::lock_guard<std::mutex> lock (mtx_);
+
+  if (queue_.empty ())
+    return nullptr;
+
+  std::unique_ptr<queue_message> m = std::move (queue_.front ());
+  if (m)
+    {
+      queue_.pop_front ();
+      return m;
+    }
+
+  return nullptr;
+}
---
+++ src/queue.hh
@@ -0,0 +1,76 @@
+// -*- mode: c++; coding: utf-8 -*-
+
+// This file is part of
+// Simple IME module for GNU Emacs (tr-emacs-ime-module)
+// https://github.com/trueroad/tr-emacs-ime-module
+//
+// Copyright (C) 2020 Masamichi Hosoda
+//
+// Simple IME module for GNU Emacs (tr-emacs-ime-module)
+// is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Simple IME module for GNU Emacs (tr-emacs-ime-module)
+// is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with tr-emacs-ime-module.
+// If not, see <https://www.gnu.org/licenses/>.
+
+#ifndef INCLUDE_GUARD_QUEUE_HH
+#define INCLUDE_GUARD_QUEUE_HH
+
+#include <deque>
+#include <memory>
+#include <mutex>
+
+class queue_message
+{
+public:
+  enum class message { setopenstatus };
+
+  explicit queue_message (message msg):
+    message_ (msg)
+  {
+  }
+  virtual ~queue_message ()
+  {
+  }
+
+  message get_message (void) const
+  {
+    return message_;
+  }
+
+private:
+  const message message_;
+};
+
+class ui_to_lisp_queue
+{
+public:
+  explicit ui_to_lisp_queue () = delete;
+  ~ui_to_lisp_queue () = delete;
+  ui_to_lisp_queue (const ui_to_lisp_queue &) = delete;
+  ui_to_lisp_queue (ui_to_lisp_queue &&) = delete;
+  ui_to_lisp_queue& operator = (const ui_to_lisp_queue &) = delete;
+  ui_to_lisp_queue& operator = (ui_to_lisp_queue &&) = delete;
+
+  static bool empty(void);
+  static void enqueue (std::unique_ptr<queue_message> &&);
+  static void enqueue_fast (std::unique_ptr<queue_message> &&);
+  static void enqueue_one (std::unique_ptr<queue_message> &&);
+  static void enqueue_fast_one (std::unique_ptr<queue_message> &&);
+  static std::unique_ptr<queue_message> dequeue (void);
+
+private:
+  static std::mutex mtx_;
+  static std::deque<std::unique_ptr<queue_message>> queue_;
+};
+
+#endif // INCLUDE_GUARD_QUEUE_HH
