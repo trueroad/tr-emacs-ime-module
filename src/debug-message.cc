0181536eb4430ab51a0bfda41b1ef9458503b204
Masamichi Hosoda <trueroad@trueroad.jp>
2020-10-24 23:26:01 +0900

    Rename package name in C++ source comments

--- src/debug-message.cc
+++ src/debug-message.cc
@@ -1,25 +1,25 @@
 // -*- mode: c++; coding: utf-8 -*-
 
 // This file is part of
-// Simple IME module for GNU Emacs (tr-emacs-ime-module)
+// Emulator of GNU Emacs IME patch for Windows (tr-ime)
 // https://github.com/trueroad/tr-emacs-ime-module
 //
 // Copyright (C) 2020 Masamichi Hosoda
 //
-// Simple IME module for GNU Emacs (tr-emacs-ime-module)
+// Emulator of GNU Emacs IME patch for Windows (tr-ime)
 // is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// Simple IME module for GNU Emacs (tr-emacs-ime-module)
+// Emulator of GNU Emacs IME patch for Windows (tr-ime)
 // is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with tr-emacs-ime-module.
+// along with tr-ime.
 // If not, see <https://www.gnu.org/licenses/>.
 
 #include "debug-message.hh"
