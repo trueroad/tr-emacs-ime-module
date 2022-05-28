4835d87b5a541861a493a7598c117f1ac3c4a0c9
Masamichi Hosoda <trueroad@trueroad.jp>
2022-05-28 14:15:49 +0900

    Add defcustom tr-ime-thread-message-dispatch-wm-timer-p
    スレッドメッセージ WM_TIMER のみをディスパッチする設定用の変数を追加。
    
    あわせて、従来の設定用変数を「すべての」スレッドメッセージを
    ディスパッチする設定とし、追加した設定の方が優先されるようにする。

--- lisp/tr-ime-thread-message.el
+++ lisp/tr-ime-thread-message.el
@@ -1,6 +1,6 @@
 ;;; tr-ime-thread-message.el --- Thread message -*- lexical-binding: t -*-
 
-;; Copyright (C) 2020 Masamichi Hosoda
+;; Copyright (C) 2020, 2022 Masamichi Hosoda
 
 ;; Author: Masamichi Hosoda <trueroad@trueroad.jp>
 ;; URL: https://github.com/trueroad/tr-emacs-ime-module
@@ -49,13 +49,23 @@
 
 (declare-function tr-ime-modadv--set-dispatch-thread-message "tr-ime-modadv"
                   (arg1))
+(declare-function tr-ime-modadv--set-dispatch-thread-wm-timer "tr-ime-modadv"
+                  (arg1))
+(defvar tr-ime-thread-message-dispatch-p)
+(defvar tr-ime-thread-message-dispatch-wm-timer-p)
 
 (defun tr-ime-thread-message--dispatch-set (symb bool)
-  "スレッドメッセージをディスパッチするか否か設定する.
+  "すべてのスレッドメッセージをディスパッチするか否か設定する.
 
 SYMB は tr-ime-thread-message-dispatch-p を設定する。
-BOOL が non-nil ならスレッドメッセージをディスパッチする。
-そうでなければスレッドメッセージをディスパッチしない。
+BOOL が non-nil ならすべてのスレッドメッセージをディスパッチする。
+そうでなければディスパッチしない。
+
+本関数の設定より
+tr-ime-thread-message--dispatch-wm-timer-set 関数の設定が優先される。
+本関数による設定を有効にしたい場合は
+tr-ime-thread-message--dispatch-wm-timer-set の BOOL を
+nil にする必要がある。
 
 GNU Emacs 27 や 28 の UI スレッドは、
 スレッドメッセージをディスパッチしないため IME の動作に不具合が発生する
@@ -63,18 +73,66 @@ GNU Emacs 27 や 28 の UI スレッドは、
 そこで、本設定によって Emacs の代わりにメッセージフックが
 スレッドメッセージをディスパッチするようにできる。
 
+大抵の場合はすべてのスレッドメッセージではなく
+WM_TIMER だけをディスパッチすればよいので、本関数ではなく
+tr-ime-thread-message--dispatch-wm-timer-set を使った方が良い。
+WM_TIMER だけではうまくいかないときに本関数を使う。
+
 ただし、将来の Emacs でスレッドメッセージをディスパッチするように修正されたら
 本設定を nil にすること。
 さもなければひとつのスレッドメッセージを
 二重にディスパッチしてしまうことになり、
 Emacs の動作がおかしくなってしまう。"
-  (tr-ime-modadv--set-dispatch-thread-message bool)
+  (if (and (boundp 'tr-ime-thread-message-dispatch-wm-timer-p)
+           (not tr-ime-thread-message-dispatch-wm-timer-p))
+      (tr-ime-modadv--set-dispatch-thread-message bool)
+    (tr-ime-modadv--set-dispatch-thread-message nil))
   (set-default symb bool))
 
-(defcustom tr-ime-thread-message-dispatch-p t
-  "スレッドメッセージをディスパッチするか否か.
+(defun tr-ime-thread-message--dispatch-wm-timer-set (symb bool)
+  "スレッドメッセージ WM_TIMER をディスパッチするか否か設定する.
+
+SYMB は tr-ime-thread-message-dispatch-wm-timer-p を設定する。
+BOOL が non-nil ならスレッドメッセージ WM_TIMER をディスパッチして
+WM_NULL にすり替えて握りつぶす。そうでなければしない。
+
+本関数の設定は tr-ime-thread-message--dispatch-set よりも優先する。
+tr-ime-thread-message--dispatch-set 関数による設定を有効にしたい場合は
+本関数の BOOL を nil にする必要がある。
+
+GNU Emacs 27 や 28 の UI スレッドは、
+スレッドメッセージをディスパッチしないため IME の動作に不具合が発生する
+（タスクトレイの IME 状態表示アイコンが変わらない等）。
+そこで、本設定によって Emacs の代わりにメッセージフックが
+スレッドメッセージ WM_TIMER をディスパッチするようにできる。
+
+大抵の場合はすべてのスレッドメッセージではなく
+WM_TIMER だけをディスパッチすればよい。
+また、WM_NULL にすり替えて握りつぶすことにより
+将来の Emacs でスレッドメッセージをディスパッチするように
+修正されても二重ディスパッチにならないようにしている。
+
+WM_TIMER だけではうまくいかない時には本関数の BOOL を nil にして
+tr-ime-thread-message--dispatch-set を使って
+すべてのスレッドメッセージをディスパッチするとよいかもしれない。"
+  (let (_dummy)
+    (if bool
+        (tr-ime-thread-message--dispatch-set '_dummy nil)
+      (if (boundp 'tr-ime-thread-message-dispatch-p)
+          (tr-ime-thread-message--dispatch-set
+           '_dummy tr-ime-thread-message-dispatch-p))))
+  (tr-ime-modadv--set-dispatch-thread-wm-timer bool)
+  (set-default symb bool))
+
+(defcustom tr-ime-thread-message-dispatch-p nil
+  "すべてのスレッドメッセージをディスパッチするか否か.
 
 この設定を変更する場合には \"custom-set-variables\" を使うこと。
+
+本設定より
+tr-ime-thread-message--dispatch-wm-timer-p 設定が優先される。
+本設定を有効にしたい場合は
+tr-ime-thread-message--dispatch-wm-timer-p を nil にする必要がある。
 
 GNU Emacs 27 や 28 の UI スレッドは、
 スレッドメッセージがディスパッチされない。
@@ -82,6 +140,11 @@ GNU Emacs 27 や 28 の UI スレッドは、
 （タスクトレイの IME 状態表示アイコンが変わらない等）。
 そこで、本設定によってメッセージフックが
 スレッドメッセージをディスパッチするようにできる。
+
+大抵の場合はすべてのスレッドメッセージではなく
+WM_TIMER だけをディスパッチすればよいので、本設定ではなく
+tr-ime-thread-message-dispatch-wm-timer-p を使った方が良い。
+WM_TIMER だけではうまくいかないときに本設定を使う。
 
 ただし、将来の Emacs で
 スレッドメッセージをディスパッチするようになったら
@@ -94,10 +157,41 @@ Emacs の動作がおかしくなってしまう。"
   :set #'tr-ime-thread-message--dispatch-set
   :group 'tr-ime-thread-message)
 
+(defcustom tr-ime-thread-message-dispatch-wm-timer-p t
+  "スレッドメッセージ WM_TIMER をディスパッチするか否か.
+
+この設定を変更する場合には \"custom-set-variables\" を使うこと。
+
+本設定は tr-ime-thread-message--dispatch-p よりも優先する。
+tr-ime-thread-message--dispatch-p の設定を有効にしたい場合は
+本設定 を nil にする必要がある。
+
+GNU Emacs 27 や 28 の UI スレッドは、
+スレッドメッセージがディスパッチされない。
+これによって IME の動作に不具合が発生する
+（タスクトレイの IME 状態表示アイコンが変わらない等）。
+そこで、本設定によってメッセージフックが
+スレッドメッセージ WM_TIMER をディスパッチするようにできる。
+
+大抵の場合はすべてのスレッドメッセージではなく
+WM_TIMER だけをディスパッチすればよい。
+また、WM_NULL にすり替えて握りつぶすことにより
+将来の Emacs でスレッドメッセージをディスパッチするように
+修正されても二重ディスパッチにならないようにしている。
+
+WM_TIMER だけではうまくいかない時には本設定を nil にして
+tr-ime-thread-message-dispatch-p を使って
+すべてのスレッドメッセージをディスパッチするとよいかもしれない。"
+  :type '(choice (const :tag "Enable" t)
+                 (const :tag "Disable" nil))
+  :set #'tr-ime-thread-message--dispatch-wm-timer-set
+  :group 'tr-ime-thread-message)
+
 (defun tr-ime-thread-message-unload-function ()
   "アンロードするためスレッドメッセージのディスパッチを無効にする."
   (let (_dummy)
-    (tr-ime-thread-message--dispatch-set '_dummy nil)))
+    (tr-ime-thread-message--dispatch-set '_dummy nil)
+    (tr-ime-thread-message--dispatch-wm-timer-set '_dummy nil)))
 
 ;;
 ;; provide
